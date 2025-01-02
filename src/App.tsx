14fc500a9d23077d5e42a54de2d9905bcc8bcfa2
Atikur Rahaman <atikurrahaman0305@gmail.com>
2025-01-02 15:15:59 +0600

    added dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import MainLayout from "./components/layout/MainLayout";
+import { ThemeProvider } from "./components/ui/theme-provider";
 
 const App = () => {
   return (
     <>
-      <MainLayout />
+      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+        <MainLayout />
+      </ThemeProvider>
     </>
   );
 };
--- src/components/layout/DashboardLayout.tsx
+++ src/components/layout/DashboardLayout.tsx
@@ -1,30 +1,8 @@
-import { Layout } from "antd";
-import { Outlet } from "react-router-dom";
-// import Navbar from "../ui/custom/others/Navbar";
-import Sidebar from "./Sidebar";
-
-const { Content } = Layout;
-
 const DashboardLayout = () => {
   return (
-    <Layout style={{ minHeight: "100vh", overflow: "hidden" }}>
-      <Sidebar />
-      <Layout>
-        {/* <Navbar /> */}
-
-        <Content style={{ margin: "24px 16px 0" }}>
-          <div
-            style={{
-              padding: 24,
-              minHeight: 360,
-              overflow: "hidden",
-            }}
-          >
-            <Outlet />
-          </div>
-        </Content>
-      </Layout>
-    </Layout>
+    <div>
+      <h1>This is dashboard layout</h1>
+    </div>
   );
 };
 
