d9e609b41327fa7fae139a71ca519b8ed1ab0da0
Atikur Rahaman <atikurrahaman0305@gmail.com>
2025-01-06 20:20:53 +0600

    added anime get with slug

--- src/pages/AddedAnime.tsx
+++ src/pages/AddedAnime.tsx
@@ -1,7 +1,21 @@
+import AddedCard from "@/components/ui/custom/AddedCard";
+import { useGetMyDataQuery } from "@/redux/features/anime/animeApi";
+import { useCurrentToken } from "@/redux/features/auth/authSlice";
+import { useAppSelector } from "@/redux/hooks";
+import { TExternalAPi } from "@/types";
+
 const AddedAnime = () => {
+  const token = useAppSelector(useCurrentToken);
+
+  const { data, isLoading, isFetching } = useGetMyDataQuery(token);
+
+  if (isLoading || isFetching) return <h1>Loading...</h1>;
+
   return (
-    <div>
-      <h1>This is AddedAnime component</h1>
+    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+      {data.data?.map((anime: TExternalAPi) => (
+        <AddedCard key={anime._id} anime={anime} />
+      ))}
     </div>
   );
 };
